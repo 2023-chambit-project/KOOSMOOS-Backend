89542e7428d94f78c6261cae825bac6c9263e3bd
yunshin <2younsin@gmail.com>
2023-09-26 22:04:48 +0900

    chore: 요청 데이터에 대한 검증 작업 추가

--- src/gallery/dtos/ReqUploadContent.dto.ts
+++ src/gallery/dtos/ReqUploadContent.dto.ts
@@ -1,6 +1,9 @@
+import { IsIn, IsUrl } from 'class-validator';
 import type { Category } from '../types';
 
 export class ReqUploadContent {
+  @IsIn(['launch', 'space', 'planet', 'AS', 'video'])
   category: Category;
+  @IsUrl()
   contentSrc: string;
 }
