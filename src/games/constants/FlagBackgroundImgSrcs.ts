e32c8ab62cca46484192e153ea6f07afb193d8ce
yunshin <2younsin@gmail.com>
2023-12-07 01:00:52 +0900

    feat: 깃발 배경 이미지 url 관리 파일 생성

---
+++ src/games/constants/FlagBackgroundImgSrcs.ts
@@ -0,0 +1,14 @@
+const FlagBackgroundImgSrcs = [
+  'https://koosmoos-test-bucket.s3.ap-northeast-2.amazonaws.com/games/flag-backgrounds/Colony.webp',
+  'https://koosmoos-test-bucket.s3.ap-northeast-2.amazonaws.com/games/flag-backgrounds/Comets.webp',
+  'https://koosmoos-test-bucket.s3.ap-northeast-2.amazonaws.com/games/flag-backgrounds/Galaxy.webp',
+  'https://koosmoos-test-bucket.s3.ap-northeast-2.amazonaws.com/games/flag-backgrounds/Jupiter.webp',
+  'https://koosmoos-test-bucket.s3.ap-northeast-2.amazonaws.com/games/flag-backgrounds/KarmaLine.webp',
+  'https://koosmoos-test-bucket.s3.ap-northeast-2.amazonaws.com/games/flag-backgrounds/MilkyWay.webp',
+  'https://koosmoos-test-bucket.s3.ap-northeast-2.amazonaws.com/games/flag-backgrounds/PeacefulPlanet.webp',
+  'https://koosmoos-test-bucket.s3.ap-northeast-2.amazonaws.com/games/flag-backgrounds/Quasar.webp',
+  'https://koosmoos-test-bucket.s3.ap-northeast-2.amazonaws.com/games/flag-backgrounds/Spaceship.webp',
+  'https://koosmoos-test-bucket.s3.ap-northeast-2.amazonaws.com/games/flag-backgrounds/SpaceShuttle.webp',
+];
+
+export default FlagBackgroundImgSrcs;
